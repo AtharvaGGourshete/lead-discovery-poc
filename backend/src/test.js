e09ed51b37386a6665d3765b166787359d231eb1
AtharvaGGourshete <goursheteatharva@gmail.com>
2026-08-02 10:47:41 +0530

    chore: remove unused test files and scripts

--- backend/src/test.js
+++
@@ -1,9 +0,0 @@
-import { findAnnualReportPDF } from "./report-engine/pdfFinder.service.js";
-
-const pdfs = await findAnnualReportPDF(
-    "https://www.infosys.com/investors/reports-filings.html"
-);
-
-console.log("Found PDFs:");
-
-console.table(pdfs);
